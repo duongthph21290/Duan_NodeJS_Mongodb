e4984368099de4bd8280a95340f8cd881f663730
Tran Hai Duong <haiduong12122003@gmail.com>
2023-03-31 00:26:54 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// Biến môi trường
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 8888;
+// Đọc được kiểu dạng json từ phía client gửi lên
+app.use(express.json());
+
+// Đọc được dữ liệu từ form và chuyển đổi thành đối tượng JS (Từ phía client gửi lên server)
+// Tham số: {extended: true} sử dụng để có thể đọc được nhiều đối tượng khác nhau
+app.use(express.urlencoded({extended: true}))
+
+//Router
+app.use('/',(req, res) => { res.send('Server chạy thành công!!!')})
+
+
+// Lắng nghe port
+app.listen(port, ()=>{
+    console.log('Server running on the port: ' + port);
+})
