9079df8edffec74fb4952838c2cb5131602a5b71
Tran Hai Duong <haiduong12122003@gmail.com>
2023-03-31 13:25:32 +0700

    update 31/03

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // Biến môi trường
 require('dotenv').config();
+const dbConnect = require('./config/dbconnect')
+const initRoutes = require('./routes')
 
 
 const app = express();
@@ -10,7 +12,13 @@ app.use(express.json());
 
 // Đọc được dữ liệu từ form và chuyển đổi thành đối tượng JS (Từ phía client gửi lên server)
 // Tham số: {extended: true} sử dụng để có thể đọc được nhiều đối tượng khác nhau
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+
+
+//Kết nối với mongodb
+dbConnect();
+
+initRoutes(app);
 
 //Router
 app.use('/',(req, res) => { res.send('Server chạy thành công!!!')})
